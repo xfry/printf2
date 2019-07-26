76a77450be55f17cdf010fbd2418558bde7954a2
xfry <fry.andrade@gmail.com>
2019-07-26 09:47:49 -0500

    feature #1: added a new function to print two numbers

---
+++ print_sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int print_sum(int, int);
+
+int main(void)
+{
+	int result = print_sum(100,100);
+	printf("result: %d\n", result);
+	return (0);
+}
+
+int print_sum(int a, int b)
+{
+	return (a+b);
+}
